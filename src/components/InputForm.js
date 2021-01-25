6774ed7d22f0ddb59a16dcefe8a5050432dd7d15
froggy <chrisdetmering@gmail.com>
2021-01-25 13:33:40 -0800

    Final code review refactor completed

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,41 +1,37 @@
 import React from "react";
+import "./InputForm.css"; 
 import { InputGroup, InputGroupAddon, Input, Button } from "reactstrap";
 
-class InputForm extends React.Component {
-  render() {
-    return (
-      <div className="data-container">
-        <form onSubmit={this.props.handleSearchSubmit}>
-          <div className="input-search-text input-row">
-            <div className="input-text-container">
-              <label className=""></label>
-              <InputGroup>
-                <Input
-                  type="text"
-                  id="searchButton"
-                  name="button"
-                  required
-                  placeholder="Enter character name"
-                />
+function InputForm(props) {
+
+  return (
+    <div className="data-container">
+      <form onSubmit={props.handleSearchSubmit}>
+        <div className="input-search-text input-row">
+          <div className="input-text-container">
+            <label className=""></label>
+            <InputGroup>
+              <Input
+                type="text"
+                id="searchButton"
+                name="button"
+                required
+                placeholder="Enter character name"
+              />
+              <InputGroupAddon addonType="append">
                 <InputGroupAddon addonType="append">
-                  <InputGroupAddon addonType="append">
-                    <Button
-                      style={{
-                        backgroundColor: "#E5E5E5",
-                        border: "none",
-                      }}
-                    >
-                      🔍
-                    </Button>
-                  </InputGroupAddon>
+                  <Button className="Button">
+                    🔍
+                  </Button>
                 </InputGroupAddon>
-              </InputGroup>
-            </div>
+              </InputGroupAddon>
+            </InputGroup>
           </div>
-        </form>
-      </div>
-    );
-  }
+        </div>
+      </form>
+    </div>
+  );
+  
 }
 
 export default InputForm;
